196bd03cd5d37cc2591c2b420f7c74978d9163ff
Shahin Hojabrian <shahin70000@gmail.com>
2014-10-04 18:49:34 +0000

    fetched and displayed google news search

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -7,6 +7,23 @@ class HomePageController < ApplicationController
   end
 
   def minia
+
+    @search = {}
+    @search[:title] = "Iran"
+    search = Google::Search::News.new :query => @search[:title]
+    
+#     @search[:result] = Net::HTTP.get_response(URI.parse(search.get_uri)).body
+
+      @search[:results] = {}
+    @json = JSON.parse(open(URI.parse(search.get_uri)).read)
+
+    @json["responseData"]["results"].each_with_index do |result, i|
+      @search[:results][i] = {}
+      @search[:results][i][:title] = result["title"].to_s
+      @search[:results][i][:content] = result["content"].to_s
+      @search[:results][i][:link] = result["signedRedirectUrl"].to_s
+    end
+  
   end
 
   private
@@ -24,5 +41,21 @@ class HomePageController < ApplicationController
         content = mark_render content_raw
     end 
   
-  
+    def find_item uri, query
+    search = Google::Search::Web.new do |search|
+      search.query = query
+      search.size = :large
+      search.each_response { print '.'; $stdout.flush }
+    end
+    search.find { |item| item.uri =~ uri }
+  end
+
+  def rank_for query
+    print "%35s " % query
+    if item = find_item(/vision\-media\.ca/, query)
+      puts " #%d" % (item.index + 1)
+    else
+      puts " Not found"
+    end
+  end
 end
